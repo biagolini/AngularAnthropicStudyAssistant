9a3ba8d9fe510a920bef701944b2ae40187ce840
Carlos Biagolini-Jr <c.biagolini@gmail.com>
2026-06-09 23:04:13 -0300

    fix: update certification description and improve markdown formatting for questions

--- src/app/core/services/export.service.ts
+++ src/app/core/services/export.service.ts
@@ -46,7 +46,7 @@ export class ExportService {
         md += `\n\n${packDomain.description}`;
       }
       questions.forEach((q, i) => {
-        md += i > 0 ? `\n\n---\n\n### Question ${i + 1}\n\n${q.review.trim()}` : `\n\n### Question ${i + 1}\n\n${q.review.trim()}`;
+        md += `\n\n---\n\n### Question ${i + 1}\n\n${q.review.trim()}`;
       });
     }
 
